8f3614139b541a94b1669f54a8ffe4d9c5966c61
tenaye123 <mulutenayee@gmail.com>
2023-05-25 20:09:29 -0400

    i hope it works

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT||80;
+const userRouter = require('./server/api/users/user.router');
+const questionRouter = require('./server/api/questions/question.router');
+const answerRouter = require('./server/api/answers/answer.router');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/questions", questionRouter);
+app.use("/api/answer",answerRouter);
+
+app.listen(port,"0.0.0.0", () => console.log(`Listening at http://localhost:${port}`));
